8007e51842a71a6af84530bfb394a1ef8fe62f2a
Tax <taxfree9090@gmail.com>
2023-03-02 11:34:13 +0900

    feat(terraform): Add terraform innit

---
+++ terraform/registry/config/backend-config.tf
@@ -0,0 +1,5 @@
+bucket                      = "<replace-with-your-s3-bucket"
+key                         = "<replace-with-your-key.tfstate>"
+region                      = "ap-northeast-2"
+encrypt                     = true
+dynamodb_table              = "<replace-with-your-table>"
---
+++ terraform/registry/vars.tfvars
@@ -0,0 +1,2 @@
+registry_name = "next-ecr-poc-repo"
+aws_region = "ap-northeast-2"
